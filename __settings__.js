dfebb90fdf506aa0eaa347b14c477a6e61dfab67
reggietee <79715267+reggietee@users.noreply.github.com>
2023-04-24 17:46:57 -0400

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1725782.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 130460224, 130460232, 130460234, 130460248, 130460251, 130460258, 130460260, 130460261 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/130460221/1/basis.wasm.js', 'wasmUrl' : 'files/assets/130460222/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/130460220/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/130460217/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/130460218/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/130460216/1/ammo.js', 'preload' : true},
+];
